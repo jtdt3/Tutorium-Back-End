b1f662a9068ac79a02c6a2aceda9c4f0f89c2f12
Kainoa Aqui <aquic@spu.edu>
2024-11-27 12:36:04 -0800

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,12 +73,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Use 'django.db.backends.mysql' for MySQL
+        'NAME': 'postgres',
+        'USER': 'tutorium',
+        'PASSWORD': 'Shootz123!',
+        'HOST': 'database-1.cd4e6oi2494d.us-west-2.rds.amazonaws.com',  # E.g., 'database-1.abcdefg123.us-east-1.rds.amazonaws.com'
+        'PORT': '5432',  # Default port for PostgreSQL (use 3306 for MySQL)
     }
 }
+
 
 
 # Password validation
