2ad402ccfd0a32ff7f1493d17a5c83ccd54df241
Jordan Tha <102616668+jtdt3@users.noreply.github.com>
2025-05-11 23:30:43 -0700

    added to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,6 +85,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.tutrio.org",
+]
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
